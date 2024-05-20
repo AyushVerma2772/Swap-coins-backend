c9ba2de8c7a2c3e9a2997b60a35db0e2e5ab7966
AyushVerma2772 <ayushhathiwan@gmail.com>
2024-05-20 15:32:51 +0530

    change 1

--- index.js
+++ index.js
@@ -15,9 +15,7 @@ const app = express();
 
 app.use(express.urlencoded());
 app.use(express.json());
-app.use(cors({
-    origin: "http://localhost:3000"
-}))
+app.use(cors());
 
 
 // connect with mongoose
